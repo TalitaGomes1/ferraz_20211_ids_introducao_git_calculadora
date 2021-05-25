1e14fc18c589fe1bc3211324eb68f72d679ec238
talita <talita.almeida878@gmail.com>
2021-05-25 08:50:53 -0300

    Faz as quatro operações basicas

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,12 @@
 a = int(input("Digite o primeiro valor "))
 b = int(input("Digite o segundo valor "))
-op = input("+: Soma\n-: Subrtração")
+op = input("+: Soma\n-: Subrtração\n*: Multiplicação\n/: Divisão")
 if op == '+':
     res = a + b
-else:
+elif op == '-':
     res = a - b
+elif op == '*':
+    res = a * b
+else:
+    res = a // b
 print(res)
