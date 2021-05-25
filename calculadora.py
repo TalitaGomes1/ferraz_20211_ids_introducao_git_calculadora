37f11b1728d6a300fa7395cd1b75a7456cd865fc
talita <talita.almeida878@gmail.com>
2021-05-25 09:41:17 -0300

    Faz exponenciacao

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,14 @@
 a = int(input("Digite o primeiro valor "))
 b = int(input("Digite o segundo valor "))
-op = input("+: Soma\n-: Subrtração\n*: Multiplicação\n/: Divisão")
+op = input("+: Soma\n-: Subrtração\n*: Multiplicação\n/: Divisão\n**: Exponenciação")
 if op == '+':
     res = a + b
 elif op == '-':
     res = a - b
 elif op == '*':
     res = a * b
-else:
+elif op == '/':
     res = a // b
+else:
+    res = a ** b
 print(res)
